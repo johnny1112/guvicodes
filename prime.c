f4a5f5a5a391f9eb25384f304f39a9ece51fbae6
johnny1112 <43337475+johnny1112@users.noreply.github.com>
2019-02-26 12:01:02 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+int flag=0;
+printf("number");
+scanf("%d",&a);
+for("i=2;i<a/2;i++)
+{
+if(a%b==0)
+{
+flag==1;
+}
+}
+if(flag==1)
+{
+printf("yes");
+}
+else
+{
+printf("no);
+}
+return 0;
+}
