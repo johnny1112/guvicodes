214820273985f158961a6ff3a1744c22297d02d8
johnny1112 <43337475+johnny1112@users.noreply.github.com>
2019-02-26 11:49:04 +0530

    Create min.c

---
+++ min.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[20];
+int i,count=0;
+printf("enter line");
+scanf("%s",a[i]);
+for(i=0;a[i]!=0;i++)
+{
+if(a[i]==' ')
+{
+count=count+1;
+}
+}
+printf("%d",count);
+getch();
+}
