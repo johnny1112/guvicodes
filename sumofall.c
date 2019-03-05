9d07537e7d39c7323306032cd3f4d1722d01005f
johnny1112 <43337475+johnny1112@users.noreply.github.com>
2019-03-05 12:29:21 +0530

    Create sumofall.c

---
+++ sumofall.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+int a,x[200],d=0,u;
+printf("value:");
+scanf("%d",&a);
+for(u;u<d;u++)
+{
+scanf("%d",&x[y]);
+}
+for(u;u<d;u++)
+{
+d=d+x[y];
+}
+printf("%d",d);
+return 0;
+}
